ca3c5b64f2beeab47a734593a85eba48b8c77a25
Marcel <marcel@unkauf.org>
2026-08-05 09:35:22 +0200

    list comprehension

--- src/chess/domain/game_state.py
+++ src/chess/domain/game_state.py
@@ -69,47 +69,43 @@ class GameStateFactory:
 
     @staticmethod
     def _init_empty_board():
-        piece_by_square = dict()
-        for file in range(0, 8):
-            for rank in range(0, 8):
-                piece_by_square[Square(file, rank)] = None
-
-        return piece_by_square
+        list_of_pairs = [(Square(file, rank), None) for file in range(8) for rank in range(8)]
+        return dict(iter(list_of_pairs))
 
     @staticmethod
     def _init_pieces(piece_by_square):
-        white_pawns = GameStateFactory._get_pieces(Pawn, 8, Color.WHITE)
+        white_pawns = GameStateFactory._create_pieces(Pawn, 8, Color.WHITE)
         for i in range(0, 8):
             GameStateFactory._init_square(Square(i, 1), white_pawns[i], piece_by_square)
-        white_rooks = GameStateFactory._get_pieces(Rook, 2, Color.WHITE)
+        white_rooks = GameStateFactory._create_pieces(Rook, 2, Color.WHITE)
         GameStateFactory._init_square(Square(0, 0), white_rooks[0], piece_by_square)
         GameStateFactory._init_square(Square(7, 0), white_rooks[1], piece_by_square)
-        white_knights = GameStateFactory._get_pieces(Knight, 2, Color.WHITE)
+        white_knights = GameStateFactory._create_pieces(Knight, 2, Color.WHITE)
         GameStateFactory._init_square(Square(1, 0), white_knights[0], piece_by_square)
         GameStateFactory._init_square(Square(6, 0), white_knights[1], piece_by_square)
-        white_bishops = GameStateFactory._get_pieces(Bishop, 2, Color.WHITE)
+        white_bishops = GameStateFactory._create_pieces(Bishop, 2, Color.WHITE)
         GameStateFactory._init_square(Square(2, 0), white_bishops[0], piece_by_square)
         GameStateFactory._init_square(Square(5, 0), white_bishops[1], piece_by_square)
         GameStateFactory._init_square(Square(3, 0), Queen(Color.WHITE), piece_by_square)
         GameStateFactory._init_square(Square(4, 0), King(Color.WHITE), piece_by_square)
 
-        black_pawns = GameStateFactory._get_pieces(Pawn, 8, Color.BLACK)
+        black_pawns = GameStateFactory._create_pieces(Pawn, 8, Color.BLACK)
         for i in range(0, 8):
             GameStateFactory._init_square(Square(i, 6), black_pawns[i], piece_by_square)
-        black_rooks = GameStateFactory._get_pieces(Rook, 2, Color.BLACK)
+        black_rooks = GameStateFactory._create_pieces(Rook, 2, Color.BLACK)
         GameStateFactory._init_square(Square(0, 7), black_rooks[0], piece_by_square)
         GameStateFactory._init_square(Square(7, 7), black_rooks[1], piece_by_square)
-        black_knights = GameStateFactory._get_pieces(Knight, 2, Color.BLACK)
+        black_knights = GameStateFactory._create_pieces(Knight, 2, Color.BLACK)
         GameStateFactory._init_square(Square(1, 7), black_knights[0], piece_by_square)
         GameStateFactory._init_square(Square(6, 7), black_knights[1], piece_by_square)
-        black_bishops = GameStateFactory._get_pieces(Bishop, 2, Color.BLACK)
+        black_bishops = GameStateFactory._create_pieces(Bishop, 2, Color.BLACK)
         GameStateFactory._init_square(Square(2, 7), black_bishops[0], piece_by_square)
         GameStateFactory._init_square(Square(5, 7), black_bishops[1], piece_by_square)
         GameStateFactory._init_square(Square(3, 7), Queen(Color.BLACK), piece_by_square)
         GameStateFactory._init_square(Square(4, 7), King(Color.BLACK), piece_by_square)
 
     @staticmethod
-    def _get_pieces(type, amount, color):
+    def _create_pieces(type, amount, color):
         pieces = list()
         count = 0
         while count < amount:
