9ca04aeaf069600ec23a94d235040e26fa0c0715
Marcel <marcel@unkauf.org>
2026-08-22 19:21:49 +0200

    test case changing turns

--- tests/domain/test_game.py
+++ tests/domain/test_game.py
@@ -1,4 +1,5 @@
 import pytest
+from chess.domain.color import Color
 from chess.domain.game import Game
 from chess.domain.pieces.pawn import Pawn
 
@@ -13,6 +14,8 @@ def test_invalid_input():
         game.make_move("a4h9")
     with pytest.raises(RuntimeError):
         game.make_move("i9b0")
+    with pytest.raises(RuntimeError):
+        game.make_move("a2b-1")
 
 def test_valid_input():
     game = Game()
@@ -20,4 +23,14 @@ def test_valid_input():
     game.make_move(input)
 
 def test_changing_turns():
-    pass
+    game = Game()
+    assert game.turn is Color.WHITE
+    assert len(game.history) == 0
+
+    game.make_move('b2b4')
+    assert game.turn is Color.BLACK
+    assert len(game.history) == 1
+
+    game.make_move('b8c6')
+    assert game.turn is Color.WHITE
+    assert len(game.history) == 2
