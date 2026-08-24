f4a8500ceb78dfb38dbf15eaf14aad00a5db3b86
Marcel <marcel@unkauf.org>
2026-08-24 20:21:40 +0200

    clean up move_validator

--- src/chess/domain/move_pattern.py
+++ src/chess/domain/move_pattern.py
@@ -2,7 +2,5 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class MovePattern:
-    direction: tuple    # Vector
+    vector: tuple 
     max_distance: int
-    # can_capture: bool
-    # can_move_without_capture: bool
--- src/chess/domain/rules/move_validator.py
+++ src/chess/domain/rules/move_validator.py
@@ -21,27 +21,10 @@ def _get_moves(source, game_state, piece):
     color = piece.color
     file = source.file
     rank = source.rank
+
     legal_moves = set()
-
     for pattern in piece.move_patterns:
-        vector = pattern.direction
-        max_distance = pattern.max_distance
-
-        for i in range(1, max_distance + 1):
-            diff_file = i * vector[0]
-            diff_rank = i * vector[1]
-            target = Square(file + diff_file, rank + diff_rank)
-            if not _is_inside_board(target):
-                break
-
-            p = game_state.get_piece(target)
-            if p is not None and p.color is color:
-                break
-            elif p is not None and p.color is not color:
-                legal_moves.add(target)
-                break
-
-            legal_moves.add(target)
+        legal_moves = legal_moves.union(_get_moves_from_vector(game_state, color, file, rank, pattern.vector, pattern.max_distance))
 
     return legal_moves
 
@@ -52,7 +35,7 @@ def _get_moves_pawn(source, game_state, pawn):
     legal_moves = set()
 
     for pattern in pawn.move_patterns:
-        vector = pattern.direction
+        vector = pattern.vector
         max_distance = pattern.max_distance
 
         if color is Color.BLACK:
@@ -61,21 +44,7 @@ def _get_moves_pawn(source, game_state, pawn):
         if pawn.in_start_position:
             max_distance = 2
 
-        for i in range(1, max_distance + 1):
-            diff_file = i * vector[0]
-            diff_rank = i * vector[1]
-            target = Square(file + diff_file, rank + diff_rank)
-            if not _is_inside_board(target):
-                break
-
-            p = game_state.get_piece(target)
-            if p is not None and p.color is color:
-                break
-            elif p is not None and p.color is not color:
-                legal_moves.add(target)
-                break
-
-            legal_moves.add(target)
+        legal_moves = legal_moves.union(_get_moves_from_vector(game_state, color, file, rank, vector, max_distance))
 
     possible_capture_one = Square(file + 1, rank + vector[1])
     if _is_inside_board(possible_capture_one) and _pawn_can_capture(game_state, color, possible_capture_one):
@@ -91,6 +60,26 @@ def _get_moves_pawn(source, game_state, pawn):
 
     return legal_moves
 
+def _get_moves_from_vector(game_state, color, file, rank, vector, max_distance):
+    legal_moves = set()
+    for i in range(1, max_distance + 1):
+        diff_file = i * vector[0]
+        diff_rank = i * vector[1]
+        target = Square(file + diff_file, rank + diff_rank)
+        if not _is_inside_board(target):
+            break
+
+        p = game_state.get_piece(target)
+        if p is not None and p.color is color:
+            break
+        elif p is not None and p.color is not color:
+            legal_moves.add(target)
+            break
+
+        legal_moves.add(target)
+
+    return legal_moves
+
 def _pawn_can_capture(game_state, color, square):
     x = game_state.get_piece(square)
     return x is not None and x.color is not color
@@ -102,9 +91,9 @@ def _en_passant_is_allowed(en_passant_square, source, pawn):
     move_pattern = pawn.move_patterns[0]
     new_rank = None
     if pawn.color is Color.WHITE:
-        new_rank = source.rank + move_pattern.direction[1]
+        new_rank = source.rank + move_pattern.vector[1]
     else:
-        new_rank = source.rank - move_pattern.direction[1]
+        new_rank = source.rank - move_pattern.vector[1]
 
     possible_en_passant_squares = set()
     possible_en_passant_squares.add(Square(source.file + 1, new_rank))
