8c8b1e65b81c35750e44cc737af7d630d4403858
Marcel <marcel@unkauf.org>
2026-09-01 15:59:46 +0200

    clean up

--- src/chess/domain/pieces/pawn.py
+++ src/chess/domain/pieces/pawn.py
@@ -6,10 +6,8 @@ class Pawn(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if self.is_dark:
-            self.movement_patterns.append(MovementPattern((0, -1), 1))
-        else:
-            self.movement_patterns.append(MovementPattern((0, 1), 1))
+        pattern = MovementPattern((0, -1), 1) if self.is_dark else MovementPattern((0, 1), 1)
+        self.movement_patterns.append(pattern)
 
     def __str__(self):
         return "P" + str(self.color.value)
