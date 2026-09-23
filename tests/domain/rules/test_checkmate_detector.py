51c0197ab33d7c88f981ad31d039dd2a5ba16ca1
Marcel <marcel@unkauf.org>
2026-09-23 10:14:00 +0200

    test checkmate detector

--- tests/domain/rules/test_checkmate_detector.py
+++ tests/domain/rules/test_checkmate_detector.py
@@ -2,12 +2,24 @@ from chess.domain.color import Color
 from chess.domain.game_state import GameStateFactory
 from chess.domain.pieces.bishop import Bishop
 from chess.domain.pieces.king import King
+from chess.domain.pieces.knight import Knight
+from chess.domain.pieces.pawn import Pawn
+from chess.domain.pieces.queen import Queen
+from chess.domain.pieces.rook import Rook
 from chess.domain.rules.checkmate_detector import game_state_is_in_checkmate
 from chess.domain.square import Square
 
 
 def test_attacker_can_be_captured():
-    pass
+    game_state = GameStateFactory.create_empty_game_state()
+    game_state.place_piece(King(Color.WHITE), Square.from_string('e1'), False)
+    game_state.place_piece(Rook(Color.WHITE), Square.from_string('d1'), False)
+    game_state.place_piece(Pawn(Color.WHITE), Square.from_string('d2'), False)
+    game_state.place_piece(Rook(Color.WHITE), Square.from_string('f1'), False)
+    game_state.place_piece(Pawn(Color.WHITE), Square.from_string('f2'), False)
+    game_state.place_piece(Knight(Color.WHITE), Square.from_string('c5'), False)
+    game_state.place_piece(Rook(Color.BLACK), Square.from_string('e4'), False)
+    assert not game_state_is_in_checkmate(game_state, Color.WHITE)
 
 def test_attacker_can_be_blocked():
     pass
